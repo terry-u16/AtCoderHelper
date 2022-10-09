5469cda39d7a5ecfe69c9840967844fe36bca68f
terry-u16 <terry-u16@users.noreply.github.com>
2022-10-09 15:34:09 +0900

    説明を英語にした

--- AtCoderHelper/Command.cs
+++ AtCoderHelper/Command.cs
@@ -5,15 +5,15 @@ namespace TerryU16.AtCoderHelper;
 
 internal class Command : ConsoleAppBase
 {
-    [Command("test", "テストケースを取得します。")]
-    public async Task GetTestCases([Option("c", "コンテスト名")] string contestName, [Option("p", "問題名")] string problemName)
+    [Command("test", "get test cases.")]
+    public async Task GetTestCases([Option("c", "contest name")] string contestName, [Option("p", "problem name")] string problemName)
     {
         var testCaseManager = new TestCaseManager();
         await testCaseManager.GetTestCaseAsync(contestName, problemName, Context.CancellationToken);
     }
 
-    [Command("format", "提出用コードを整形します。")]
-    public async Task FormatCode([Option("p", "問題名")] string problemName)
+    [Command("format", "format code for submit.")]
+    public async Task FormatCode([Option("p", "problem name")] string problemName)
     {
         var formatter = new CodeFormatter();
         await formatter.ConcatAsync(problemName, Context.CancellationToken);
