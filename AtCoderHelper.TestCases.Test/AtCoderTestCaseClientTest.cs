44b30ed55b02e6e2cf149dc282641b47c58226c0
terry-u16 <terry-u16@users.noreply.github.com>
2022-10-09 13:28:19 +0900

    AtCoderTestCaseClientを追加

---
+++ AtCoderHelper.TestCases.Test/AtCoderTestCaseClientTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerryU16.AtCoderHelper.TestCases.Test;
+
+public class AtCoderTestCaseClientTest
+{
+    [Fact]
+    public async Task GetTestCasesAsyncTest()
+    {
+        var client = new AtCoderTestCaseClient();
+
+        var testCases = await client.GetTestCasesAsync("abc271", "a");
+
+        var expected = new TestCase[]
+        {
+            new TestCase("99", "63"),
+            new TestCase("12", "0C"),
+            new TestCase("0", "00"),
+            new TestCase("255", "FF"),
+        };
+
+        Assert.Equal(expected, testCases);
+    }
+}
